76f620a0a3e08796584c6d332a13b0992b53c05c
egor-moroz <egorlex@gmail.com>
2017-03-04 11:45:16 +0200

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { Http, Response } from '@angular/http'
 
 @Component({
-  selector: 'app-root',
+  selector: 'http-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  data: Object;
+  loading: boolean;
+
+constructor(private http: Http){
+
+}
+makeRequest(): void {
+  this.loading = true;
+  this.http.request('http://jsonplaceholder.typicode.com/posts/1')
+  .subscribe((res: Response) => {
+    this.data = res.json();
+    this.loading = false;
+  });
+}
+
 }
